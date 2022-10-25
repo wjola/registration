afb3694bc0f54bdfda7f5f9577c33ee259c80c0c
Wioletta <wioletta.czardybon@bidroom.com>
2022-10-25 18:46:03 +0200

    Add submit button

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,10 +1,18 @@
 import FormInput from "./FormInput";
+import Button from "./Button";
 
 const RegistrationForm = () => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <form>
-      <FormInput type="text" id="name" label="Imię" />
-      <FormInput type="password" id="password" label="Hasło" />
+      <div>
+        <FormInput type="text" id="name" label="Imię" />
+        <FormInput type="password" id="password" label="Hasło" />
+      </div>
+      <Button text="Zarejestruj" onClick={onSubmit} />
     </form>
   );
 }
